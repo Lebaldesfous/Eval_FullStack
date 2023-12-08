f53cd538ea506f57ae11b2d11283fd3503a86c53
Lebaldesfous <guillaume-huet@hotmail.com>
2023-12-08 17:00:21 +0100

    fin du jeu

---
+++ Eval_fullstack/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `TAG` (`id`, `nom`) VALUES (1, "Gatronomique");
+INSERT INTO `TAG` (`id`, `nom`) VALUES (2, "Bistrot");
+INSERT INTO `TAG` (`id`, `nom`) VALUES (3, "Bistronomique");
+INSERT INTO `TAG` (`id`, `nom`) VALUES (4, "Brasserie");
+INSERT INTO `TAG` (`id`, `nom`) VALUES (5, "Fastfood");
